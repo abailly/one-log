ac8f39a92990d80c78a8b2e46ac758ad6a520921
Arnaud Bailly <arnaud.oqube@gmail.com>
2018-04-21 10:41:38 +0200

    try to convince java payment service to log as JSON

--- Build.hs
+++ Build.hs
@@ -41,7 +41,7 @@ doBuild = do
 
 doRun :: IO ()
 doRun = do
-  j <- spawnProc "java" [ "-jar" , "./pet-store-payment/target/pet-store-payment-1.0-SNAPSHOT.jar", "server" ] "."
+  j <- spawnProc "java" [ "-jar" , "./pet-store-payment/target/pet-store-payment-1.0-SNAPSHOT.jar", "server", "payment-conf.yaml" ] "."
   h <- spawnProc "stack" [ "exec" ,"pet-store-server", "--", "Prod" , "9090", "localhost", "8080" ] "."
   void $ waitAnyCancel [ j, h]
 
