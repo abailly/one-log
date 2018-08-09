4156ef0693f0fbcfd715dafb07bb0076b346110a
Arnaud Bailly <arnaud.oqube@gmail.com>
2018-08-09 10:53:38 +0200

    provide CLI arguments for driver

--- pet-store/driver/driver.hs
+++ pet-store/driver/driver.hs
@@ -1,9 +1,23 @@
-{-# LANGUAGE ViewPatterns #-}
-
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE RecordWildCards    #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeOperators      #-}
+import           Options.Generic
 import           PetStore.Driver
-import           System.Environment
+
+data Options w = Options { numThreads :: w ::: Int <?> "Number of threads (e.g. clients) to run driver with"
+                         , serverHost :: w ::: String <?> "Hostname/IP of server"
+                         , serverPort :: w ::: Int <?> "Port of server"
+                         }
+    deriving (Generic)
+
+instance ParseRecord (Options Wrapped)
+deriving instance Show (Options Unwrapped)
 
 main :: IO ()
 main = do
-  [read -> numThreads, serverHost, serverPort] <- getArgs -- to connect to server
-  runTestDriver numThreads serverHost (read serverPort)
+  Options{..} <- unwrapRecord "Pet-Store Driver"
+  runTestDriver numThreads serverHost serverPort
